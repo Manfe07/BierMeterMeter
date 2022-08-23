0be6c4eac7f940c818da4442d2960f20b7049135
Manuel Fehren <Manuel.Fehren@Gmail.com>
2022-08-23 19:08:06 +0200

    Update datahandler.py

--- items/datahandler.py
+++ items/datahandler.py
@@ -1,6 +1,6 @@
 import sqlite3
 import settings
-import pprint
+from pprint import pprint
 
 db_file = settings.db_file
 
@@ -14,6 +14,17 @@ def init():
                 id INTEGER PRIMARY KEY,
                 name VARCHAR(100) UNIQUE NOT NULL,
                 price DECIMAL(5,2),
+                group_id INTEGER,
+                editor VARCHAR(100)
+            );
+        ''')
+
+        #Create Table "item_groups"
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS item_groups (
+                id INTEGER PRIMARY KEY,
+                name VARCHAR(100) UNIQUE NOT NULL,
+                color VARCHAR(32),
                 editor VARCHAR(100)
             );
         ''')
@@ -25,33 +36,97 @@ def get_Items():
         con = sqlite3.connect(db_file)
         cur = con.cursor()
 
+        groups = getGroups(asDic=True)
+
         list = []
         result = cur.execute('SELECT * FROM items ORDER BY name ASC')
         try:
                 for row in result:
-                        pprint.pprint(row)
+                        if row[3]:
+                                group_id = row[3]
+                        else:
+                                group_id = 1
+
                         list.append({
-                                "id":row[0],
-                                "name":row[1],
-                                "price":float(row[2]),
-                                "editor":row[3]
+                                "id" : row[0],
+                                "name" : row[1],
+                                "price" : float(row[2]),
+                                "group" : groups[group_id],
+                                "editor" : row[4]
                         })
         except Exception as e:
                 print(e)
 
         con.close()
+        pprint(list)
         return list
 
 
-def addItem(name, price=None, editor=None):
+def addItem(name, price=None, editor=None, group_id=1):
         con = sqlite3.connect(db_file)
         cur = con.cursor()
         try:
-                cur.execute("INSERT INTO items(name, price, editor) VALUES (?,?,?)", (name, price, editor))
+                cur.execute("INSERT INTO items(name, price, group_id, editor) VALUES (?,?,?,?)", (name, price, group_id, editor))
         except Exception as e:
                 print(e)
         con.commit()
         con.close()
+
+
+def getGroups(asDic = False):
+        con = sqlite3.connect(db_file)
+        cur = con.cursor()
+
+        result = cur.execute('SELECT * FROM item_groups ORDER BY name ASC')
+
+        if asDic:
+                dic = {}
+                try:
+                        for row in result:
+                                id = row[0]
+                                name = row[1]
+                                color = row[2]
+                                editor = row[3]
+
+                                dic[id] = {
+                                        "id" : id,
+                                        "name" : name,
+                                        "color" : color,
+                                        "editor" : editor,
+                                }
+                except Exception as e:
+                       print(e)
+
+                con.close()
+                return dic
+
+        else:
+                list = []
+                try:
+                        for row in result:
+                                list.append({
+                                        "id":row[0],
+                                        "name":row[1],
+                                        "color":row[2],
+                                        "editor":row[3]
+                                })
+                except Exception as e:
+                        print(e)
+
+                con.close()
+                return list
+
+
+def addGroup(name, color="#5294ff", editor=None):
+        con = sqlite3.connect(db_file)
+        cur = con.cursor()
+        try:
+                cur.execute("INSERT INTO item_groups(name, color, editor) VALUES (?,?,?)", (name, color, editor))
+        except Exception as e:
+                print(e)
+        con.commit()
+        con.close()
+
 
 def deleteItem(id):
         con = sqlite3.connect(db_file)
