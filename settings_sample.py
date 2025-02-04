feb1416c7f69fafc5ec21375fdd80c9ddd08a06c
Manuel Fehren <manuel.fehren@gmail.com>
2025-02-04 19:44:11 +0100

    removed old files

--- settings_sample.py
+++
@@ -1,8 +0,0 @@
-from datetime import timedelta
-
-debug = False
-port = 5000
-db_file = 'bmm.db'
-secret_key = "fcc3ab5debba5d574c53b5760bdf5822c8e05"
-hideRanking = False
-session_lifetime = timedelta(hours=6)
